c1e0b9227217a4850560d41d01ba4ee48c983777
AlejandroWaiz <alejosaurio1@gmail.com>
2021-03-06 19:32:56 -0300

    first and last commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AlejandroWaiz/email-sender
+
+go 1.15
+
+require gopkg.in/mail.v2 v2.3.1 // indirect
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+
+	gomail "gopkg.in/mail.v2"
+)
+
+func main() {
+	m := gomail.NewMessage()
+
+	// Set E-Mail sender
+	m.SetHeader("From", "alejandronicolas.waiz@gmail.com")
+
+	// Set E-Mail receivers
+	m.SetHeader("To", "alejandronicolas.waiz@gmail.com")
+
+	// Set E-Mail subject
+	m.SetHeader("Subject", "Gomail test subject")
+
+	// Set E-Mail body. You can set plain text or html with text/html
+	m.SetBody("text/plain", "Hello there, this is an example. If you can read this, congratulations! You made it.")
+
+	// Settings for SMTP server
+	d := gomail.NewDialer("smtp.gmail.com", 587, "alejandronicolas.waiz@gmail.com", "olakease123")
+
+	// This is only needed when SSL/TLS certificate is not valid on server.
+	// For production this should be set to false.
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	// Now send E-Mail
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	return
+}
